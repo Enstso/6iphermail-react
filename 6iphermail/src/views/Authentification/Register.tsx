b4d58470844db5e20928bce5557cd7a141b20783
Enstso <enstso@outlook.fr>
2024-05-25 17:11:25 +0200

    start login svg

---
+++ 6iphermail/src/views/Authentification/Register.tsx
@@ -0,0 +1,92 @@
+import { AuthRegisterForm } from "@/components/authentification/auth-register-form";
+import React from "react";
+import { cn } from "@/lib/utils";
+import { Link } from "react-router-dom";
+import { buttonVariants } from "@/components/ui/button";
+import img1 from "./authentication-dark.png";
+import img2 from "./authentication-light.png";
+import logo from "@/assets/logo.png";
+
+export default function Register() {
+  return (
+    <>
+      <div className="md:hidden">
+        <img
+          src={img1}
+          width={1280}
+          height={843}
+          alt="Authentication"
+          className="block dark:hidden"
+        />
+        <img
+          src={img2}
+          width={1280}
+          height={843}
+          alt="Authentication"
+          className="hidden dark:block"
+        />
+      </div>
+      <div className="container relative hidden h-[800px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+        <Link
+          to="#"
+          className={cn(
+            buttonVariants({ variant: "ghost" }),
+            "absolute right-4 top-4 md:right-8 md:top-8"
+          )}
+        >
+          Login
+        </Link>
+        <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
+          <div className="absolute inset-0 bg-zinc-900" />
+          <div className="relative z-20 flex items-center text-lg font-medium">
+            <img src={logo} alt="logo" height={70} width={70} />
+            <span> 6iphermail</span>
+          </div>
+          <div className="relative z-20 mt-auto">
+            <blockquote className="space-y-2">
+              <p className="text-lg">
+                &ldquo;The project aims to develop an innovative anti-phishing
+                email system that uses advanced AI and machine learning to
+                detect and prevent phishing attacks. It analyzes incoming
+                emails, identifies phishing attempts, and alerts users in
+                real-time, ensuring secure electronic communications with a
+                user-friendly interface.&rdquo;
+              </p>
+              <footer className="text-sm">6iphermail</footer>
+            </blockquote>
+          </div>
+        </div>
+        <div className="lg:p-8">
+          <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+            <div className="flex flex-col space-y-2 text-center">
+              <h1 className="text-2xl font-semibold tracking-tight">
+                Create an account
+              </h1>
+              <p className="text-sm text-muted-foreground">
+                Enter your email below to create your account
+              </p>
+            </div>
+            <AuthRegisterForm />
+            <p className="px-8 text-center text-sm text-muted-foreground">
+              By clicking continue, you agree to our{" "}
+              <Link
+                to="#"
+                className="underline underline-offset-4 hover:text-primary"
+              >
+                Terms of Service
+              </Link>{" "}
+              and{" "}
+              <Link
+                to="#"
+                className="underline underline-offset-4 hover:text-primary"
+              >
+                Privacy Policy
+              </Link>
+              .
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
